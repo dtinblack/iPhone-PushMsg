033047f64e887a4b5c4836e0f1836259c1791a03
David Black <dtinblack@gmail.com>
2014-07-01 11:16:34 +0100

    Initial Commit

---
+++ iPhone-PushMsg/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iPhone-PushMsg
+//
+//  Created by David Black on 01/07/2014.
+//  Copyright (c) 2014 David Black. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iPhone-PushMsg/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iPhone-PushMsg
+//
+//  Created by David Black on 01/07/2014.
+//  Copyright (c) 2014 David Black. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
